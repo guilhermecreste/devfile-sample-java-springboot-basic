421f24a0be15fa6826746b1373693927eeb1fda3
Guilherme Creste <guilherme_creste@yahoo.com.br>
2023-02-26 17:53:02 +0000

    Added AppDynamics integration

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,8 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @SpringBootApplication
 public class DemoApplication {
-
+    private final static Logger logger = LoggerFactory.getLogger(DemoApplication.class);
+    
     @RequestMapping("/")
     String home() {
+        logger.info("Request received.");
         return "Hello World!";
     }
 
